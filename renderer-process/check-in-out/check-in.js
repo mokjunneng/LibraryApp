9b6820abc57e51dd2e2b2728cb79d82203c78747
silentfatez <keith_goh@silentfatez.com>
2018-10-27 14:30:37 +0800

    made changes with regards to edit

--- renderer-process/check-in-out/check-in.js
+++ renderer-process/check-in-out/check-in.js
@@ -3,9 +3,6 @@ function show(p1,p2) {
   document.getElementById(p2).style.display='none';
   return false;
 }
-// var page1=document.getElementById("Page1")
-// var page2=document.getElementById("Page2")
-
 
 var shown = document.getElementById("return-button");
 shown.addEventListener("click", ()=> { show("Page2","Page1") });
