66463bbb0cae4b795b62dfdf47a8f1add17836b0
LinHuiqing <lin.huiqing@dhs.sg>
2018-11-04 23:09:17 +0800

    Searchable table pulled from database, and skeleton of add books f(x).

--- renderer-process/directory/book-directory.js
+++ renderer-process/directory/book-directory.js
@@ -1,57 +1,74 @@
-function showRelevantEntries() {
-    // Declare variables
-    var input, filter, ul, li, a, i;
-    input = document.getElementById('searchInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("directoryList");
-    li = ul.getElementsByTagName('li');
+const database = require("./models/db.js"); //../../models/databases/db
 
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-            //li[i].style.visibility = "visible";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
-
-var searchInput = document.getElementById("searchInput");
-searchInput.addEventListener("keyup", showRelevantEntries);
-
-var accordions = document.getElementsByClassName("accordion");
-
-for (var i = 0; i < accordions.length; i++) {
-  accordions[i].onclick = function() {
-    this.classList.toggle('is-open');
-
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      // accordion is currently open, so close it
-      content.style.maxHeight = null;
+database.db.getBooks().then(books => {
+  //var table = document.createElement("TABLE");
+  books.forEach(book => {
+    // var table;
+    console.log(book);
+    // console.log(book.book_id);
+    var tr = document.createElement("TR");
+    var td = document.createElement("TD");
+    td.appendChild(document.createTextNode(Object.values(book)[0]));
+    tr.appendChild(td);
+    var td = document.createElement("TD");
+    td.appendChild(document.createTextNode(Object.values(book)[1]));
+    tr.appendChild(td);
+    var td = document.createElement("TD");
+    td.appendChild(document.createTextNode(Object.values(book)[2]));
+    tr.appendChild(td);
+    var td = document.createElement("TD");
+    td.appendChild(document.createTextNode(Object.values(book)[3]));
+    tr.appendChild(td);
+    var td = document.createElement("TD");
+    if (Object.values(book)[4] == 0 ){
+      td.appendChild(document.createTextNode("-"));
     } else {
-      // accordion is currently closed, so open it
-      content.style.maxHeight = content.scrollHeight + 100 + "px";
+      td.appendChild(document.createTextNode(Object.values(book)[4]));
     }
-  }
+    tr.appendChild(td);
+    //console.log(tr);
+    //table.appendChild(tr);
+    document.getElementById("book-catalogue").appendChild(tr);
+  })
+})
+
+function searchFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, j;
+    var countAll = 0;
+    var countAppear = 0;
+    input = document.getElementById("book-search-input");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("book-catalogue");
+    tr = table.getElementsByTagName("tr");
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 1; i < table.rows.length; i++) {
+      for (j = 0; j < 5; j++){
+        name = table.rows[i].cells[j].innerHTML;
+        // Can consider searching by IC
+        if (name) {
+            if (name.toUpperCase().indexOf(filter) > -1) {
+              countAll++;
+              countAppear++;
+              console.log(name);
+              console.log("countAll = " + countAll);
+              console.log("countAppear = " + countAppear);
+            } else {
+              countAll++;
+              }
+              if (countAll == 5){
+                if (countAppear != 0){
+                  tr[i].style.display = "";
+                } else {
+                  tr[i].style.display = "none";
+                }
+              }
+            }
+        }
+        countAll = 0;
+        countAppear = 0;
+    }
 }
 
-// var accordions = bulmaAccordion.attach(); // accordions now contains an array of all Accordion instances
-//
-// var coll = document.getElementsByClassName("collapsible");
-// var i;
-//
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     console.log("hi");
-//     var collapsible-content = this.nextElementSibling;
-//     if (collapsible-content.style.maxHeight){
-//       collapsible-content.style.maxHeight = null;
-//     } else {
-//       collapsible-content.style.maxHeight = content.scrollHeight + "px";
-//     }
-//   });
-// }
+input = document.getElementById("book-search-input");
+input.addEventListener("keyup", searchFunction);
