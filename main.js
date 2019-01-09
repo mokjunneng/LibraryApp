49dee8ff027931393c5ef0ca4fad692f8311a82e
silentfatez <keith_goh@silentfatez.com>
2019-01-09 22:02:32 +0700

    auto update functionality

--- main.js
+++ main.js
@@ -1,77 +1,46 @@
-const { app, BrowserWindow, globalShortcut } = require('electron');
-const { autoUpdater } = require("electron-updater");
-const log = require('electron-log');
+// boiletplate for starting a BrowserWindow process
 
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
+const { app, BrowserWindow } = require('electron')
+const db = require('./models/db')
 
-let win;
+let win
 
 function createWindow() {
   const windowOptions = {
     width: 1080,
     minWidth: 680,
     height: 840,
-  };
-  win = new BrowserWindow(windowOptions);
+  }
 
-  win.loadFile('index.html');
+  win = new BrowserWindow(windowOptions)
 
-  win.webContents.openDevTools();
+  win.loadFile('index.html')
 
-  globalShortcut.register('f5', () => {
-    win.reload();
-  });
+  win.webContents.openDevTools()
 
   win.on('closed', () => {
     win = null
   })
-
-  return win
-};
-function sendStatusToWindow(text) {
-  log.info(text);
-  win.webContents.send('message', text);
 }
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Update available.');
-})
-autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Error in auto-updater. ' + err);
-})
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message);
-})
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
-});
 
-app.on('ready', function() {
-  createWindow();
-});
+app.on('ready', createWindow)
 
-app.on('window-all-closed', () => {
+app.on('window-all-close', () => {
   if (process.platform !== 'darwin') {
+    // close the database connection
+    db.close((err) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      console.log('Close the database connection.');
+    });
+
     app.quit()
-    globalShortcut.unregisterAll(win)
   }
-});
-app.on('ready', function()  {
-  autoUpdater.checkForUpdatesAndNotify();
-});
+})
 
 app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
-});
+})
